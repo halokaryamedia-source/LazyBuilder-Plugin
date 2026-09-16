6573d44386e6749da6506528cb635e735a4fb9ba
halokaryamedia-source <halokaryamedia@gmail.com>
2026-09-17 01:51:31 +0700

    refactor(chat): restore route model on latest Local

---
+++ mods/utility-manager/src/main/java/com/halokaryamedia/lazybuilder/utility/chat/ChatRoute.java
@@ -0,0 +1,9 @@
+package com.halokaryamedia.lazybuilder.utility.chat;
+
+/** Output surfaces available to Utility Manager after message classification. */
+public enum ChatRoute {
+    CHAT,
+    TOAST,
+    CONSOLE,
+    SUPPRESS
+}
